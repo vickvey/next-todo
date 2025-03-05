895992e17da7b56aee14d22af79ff82426244bdd
vickvey <vickvey0303@gmail.com>
2025-03-05 11:10:45 +0530

    completed prisma setup

---
+++ prisma/migrations/20250305053459_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "desc" TEXT NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'PENDING'
+);
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,6 @@
-import { Prisma } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const taskData: Prisma.TaskCreateInput[] = [
   { desc: 'Brush Your Teeth' },
@@ -6,4 +8,22 @@ const taskData: Prisma.TaskCreateInput[] = [
   { desc: 'Make a healthy meal' },
 ]
 
-async function main() {}
+async function main() {
+  console.log(`Database seeding started ...`)
+  for (const t of taskData) {
+    const task = await prisma.task.create({
+      data: t,
+    })
+    console.log(`Task with ID: ${task.id} created successfully!`)
+  }
+  console.log(`Database seeding finished!`)
+  return
+}
+
+main()
+  .then(async () => await prisma.$disconnect())
+  .catch(err => {
+    console.error('Error seeding the database: ', err)
+    process.exit(1)
+  })
+  .finally(async () => await prisma.$disconnect())
